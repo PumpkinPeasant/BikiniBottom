79cce6bbcca91bec16613c05294ebe1b2b5bf8d2
PumpkinPeasant <50501903+PumpkinPeasant@users.noreply.github.com>
2020-08-12 22:39:34 +0300

    gui

---
+++ src/main/java/ru/indproject/bikinibottom/blocks/PineappleBlock.java
@@ -0,0 +1,16 @@
+package ru.indproject.bikinibottom.blocks;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+
+public class PineappleBlock extends Block {
+
+    public PineappleBlock() {
+        super(AbstractBlock.Properties.create(Material.WOOD)
+                .hardnessAndResistance(2.0f,4.5f)
+                .sound(SoundType.WET_GRASS)
+        );
+    }
+}
--- src/main/java/ru/indproject/bikinibottom/items/ItemBase.java
+++ src/main/java/ru/indproject/bikinibottom/items/ItemBase.java
@@ -2,10 +2,11 @@ package ru.indproject.bikinibottom.items;
 
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import ru.indproject.bikinibottom.BikiniBottom;
 
 public class ItemBase extends Item {
 
     public ItemBase() {
-        super(new Item.Properties().group(ItemGroup.TOOLS));
+        super(new Item.Properties().group(BikiniBottom.TAB));
     }
 }
